f1be48f9ec9dfb8dbaa76a29b3d1754fbec917c2
Spoorthy Nalumachu <spoorthynal@g.ucla.edu>
2025-12-23 15:19:29 -0800

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <a href="#about" className="nav-link">About</a>
+        <a href="#work" className="nav-link">Work</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="home-container">
+        <h1 className="main-title">Spoorthy's Cloud</h1>
+      </main>
+    </>
+  );
+}
+
+export default Home;

eb17aff32e3da14cc7965297701a6d64e0d60c2e
Spoorthy Nalumachu <spoorthynal@g.ucla.edu>
2025-12-23 16:42:35 -0800

    Update home page: Add animated cloud background, center title and cloud, improve responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <a href="#about" className="nav-link">About</a>
-        <a href="#work" className="nav-link">Work</a>
+        <a href="Home" className="nav-link">Home</a>
+        <a href="About" className="nav-link">About</a>
+        <a href="Work" className="nav-link">Work</a>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import Navbar from "../components/Navbar";
+import cloudImage from "../assets/cloud.png";
+import "./Home.css";
 
 function Home() {
   return (
     <>
       <Navbar />
       <main className="home-container">
-        <h1 className="main-title">Spoorthy's Cloud</h1>
+        <div className="title-cloud-wrapper">
+          <h1 className="main-title">Spoorthy's Cloud</h1>
+          <div className="cloud-wrapper">
+            <img src={cloudImage} alt="Cloud" className="main-cloud" />
+          </div>
+        </div>
+        
+        {/* Background clouds */}
+        <div className="background-clouds">
+          <img src={cloudImage} alt="Cloud" className="bg-cloud cloud-1" />
+          <img src={cloudImage} alt="Cloud" className="bg-cloud cloud-2" />
+          <img src={cloudImage} alt="Cloud" className="bg-cloud cloud-3" />
+          <img src={cloudImage} alt="Cloud" className="bg-cloud cloud-4" />
+          <img src={cloudImage} alt="Cloud" className="bg-cloud cloud-5" />
+          <img src={cloudImage} alt="Cloud" className="bg-cloud cloud-6" />
+        </div>
       </main>
     </>
   );
